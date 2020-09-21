32d5315e97da65086ca2c6b04999743164676604
LucSedirae <jondeavers@gmail.com>
2020-09-20 23:34:14 -0400

    Added local weather function

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,8 +5,10 @@ var currentHour = moment().hour();
 //This variable grabs the current date using moment.js
 const m = moment().format("dddd, MMMM Do YYYY");
 //This array collects the appointment lobels so they can be indexed when pulling from localstorage
-const appointments = ["appt9", "appt10", "appt11", "appt12", "appt13", "appt14", "appt15", "appt16", "appt17"]
-
+const appointments = ["appt9", "appt10", "appt11", "appt12", "appt13", "appt14", "appt15", "appt16", "appt17"];
+//These variables store user's location data
+var latitude = "";
+var longitude = "";
 //This function indexes the appointment elements in the html and assigns local storage values
 //that correspond with the appropriate keys in the appointments array. It then calls the function that
 //checks the weather and then prints that information to the DOM
@@ -17,12 +19,10 @@ function init() {
             $(".appointment").eq(j-9).val(localStorage.getItem(appointments[j-9]));
         }
     });
-    checkWeather();
+    getLocation();
 }
-
 //prints today's date to the header
 todayDateEl.innerHTML = (m + "<br>");
-
 //changes the color of the time block based on what time of day it is currently
 for (var i = 9; i < 18; i++) {
     if (currentHour > i){
@@ -35,7 +35,6 @@ for (var i = 9; i < 18; i++) {
         $(".timeValue").eq(i-9).css("background-color", "green");
     }
 }
-
 //Save Icon listener waits for a click on a save icon then searches that element's sibling elements
 //to determine which hour the appointment is saved to and what the content of the appointment's details are
 $(".saveIcon").click(function(){
@@ -44,7 +43,6 @@ $(".saveIcon").click(function(){
 
     localStorage.setItem(timeSlot, appointment);
 });
-
 //This listener's function listens for a click on the recycle button and checks whether an appointment 
 //box has any content loaded. If so, it removes that content and refreshes the page. 
 $(".recycleIcon").click(function(){
@@ -56,18 +54,36 @@ $(".recycleIcon").click(function(){
         init();
     };
 });
-
+//This function rounds the raw coordinates gathered by getLocation() and inserts those
+//rounded values into an openweathermap API call. Finally it references that weather data and prints
+//helpful information to the DOM.
 function checkWeather() {
     // 22ce314bdb5cf097792a93d02ec2e354 Open Weather API key
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Richmond&appid=22ce314bdb5cf097792a93d02ec2e354"
+    var roundedLat = Math.round(latitude * 1000) / 1000;
+    var roundedLong = Math.round(longitude * 1000) / 1000;
+
+    console.log(roundedLat);
+    console.log(roundedLong);
+    var queryURL = ("https://api.openweathermap.org/data/2.5/weather?lat="+roundedLat+"&lon="+roundedLong+"&appid=22ce314bdb5cf097792a93d02ec2e354");
+    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
             var temperature = Math.floor((response.main.temp)-273.15) * 9/5 + 32;
-            var skyState = response.weather[0].description;
-            $("#currentWeather").text("It is currently " + temperature + " degrees and " + skyState);
-            console.log(response);
+            var skyState = (response.weather[0].description);
             console.log(temperature);
+            console.log(skyState);
+            $("#currentWeather").text("Currently " + temperature + " degrees and " + skyState);
         }
 )};
+//This function grabs the user's latitude and longitude
+function getLocation() {
+    var userCoords = navigator.geolocation.getCurrentPosition(function(position){
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
+        console.log(latitude);
+        console.log(longitude);
+    });
+    checkWeather();
+  }
